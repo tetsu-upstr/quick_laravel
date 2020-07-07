25621a1f716d2f73c58d301de4e8ad7f5edfbf07
tetsu-upstr <bodomu@gmail.com>
2020-07-08 04:41:38 +0900

    modified list.blade

--- resources/views/hello/list.blade.php
+++ resources/views/hello/list.blade.php
@@ -7,7 +7,8 @@
   <link rel="stylesheet" href="http://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/boostrap.min.css" />
 </head>
 <body>
-<table class="table">
+
+<!-- <table class="table">
   <tr>
     <th>書名</th>
     <th>価格</th>
@@ -22,6 +23,27 @@
     <td>{{ $record->published }}</td>
   </tr>
 @endforeach
+</table> -->
+
+<table class="table">
+  <tr>
+    <th>No.</th>
+    <th>書名</th>
+    <th>価格</th>
+    <th>出版社</th>
+    <th>刊行日</th>
+  </tr>
+<!-- インデックス番号を取り出す -->
+@foreach($records as $id => $record)
+  <tr>
+    <td>{{ $id + 1 }}</td>
+    <td>{{ $record->title }}</td>
+    <td>{{ $record->price }}円</td>
+    <td>{{ $record->publisher }}</td>
+    <td>{{ $record->published }}</td>
+  </tr>
+@endforeach
 </table>
+
 </body>
 </html>
