d9c760dc07b50b4da3ff92efeb7290007dd96eea
tetsu-upstr <bodomu@gmail.com>
2020-07-10 13:03:44 +0900

    redirect

--- app/Http/Controllers/CtrlController.php
+++ app/Http/Controllers/CtrlController.php
@@ -33,4 +33,39 @@ class CtrlContoller extends Controller
                 ])
                 ->withCallback('callback');
     }
+
+    // ファイルのダウンロード
+    public function outFile()
+    {
+        return response()
+                ->download('C:/Data/data_log.csv', 'download.csv', ['Content-Type' => 'text/csv']);
+    }
+
+    // 生成したデータをダウンロード
+    public function outCsv()
+    {
+        return response()
+                ->streamDownload(function() {
+                    print(
+                        "1,2019/10/1,123\n".
+                        "2,2019/10/2,116\n".
+                        "3,2019/10/3,98\n".
+                        "4,2019/10/4,102\n".
+                        "5,2019/10/5,134\n"
+                    );
+                }, 'download.csv', ['Content-Type' => 'text/csv']);
+    }
+
+    // 指定されたファイルを出力してブラウザに描画
+    public function outImage()
+    {
+        return response()
+                ->file('C:/Data/wings.png', ['Content-Type' => 'image/png']);
+    }
+
+    // リダイレクト処理
+    public function redirectBasicr()
+    {
+        return redirect('hello/list');
+    }
 }
